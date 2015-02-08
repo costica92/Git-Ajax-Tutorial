52b8f2916aa6412745c341dcb85f05ed6accbadf
Breahna Constantin <costica92@gmail.com>
2015-02-08 23:05:45 +0200

    git ajax tutorial

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
-$(funtion(){
+$(function(){
+
+	var $orders = $('#orders');
+	var $name = $('#name');
+	var $drink = $('#drink');
+
 	$.ajax({
 		type: 'GET',
-		url: 'http://rest.learncode.academy/api/johnbob/friends'
+		url: 'http://rest.learncode.academy/api/johnbob/orders',
+		success: function(friends) {			
+			$.each(friends, function(i, friend){
+				$orders.append('<li>name: '+ friend.name +', drink: '+ friend.drink +' </li>');
+			});
+		}, error: function(){
+			alert('error loading order');
+		}
 	});
+
+
+$('#add-order').on('click', function(){
+
+	var order = {
+		name: $name.val(),
+		drink = $drink.val()
+	};
+
+	$.ajax({
+		type: 'POST',
+		url: 'http://rest.learncode.academy/api/johnbob/orders',
+		data: order,
+		success: function(newOrder){
+			$orders.append('<li>name: '+ newOrder.name +', drink: '+ newOrder.drink +' </li>');
+		},
+		error: function(){
+			alert('error saving order');
+		}
+	});
+
+});
+
+
+
 });
